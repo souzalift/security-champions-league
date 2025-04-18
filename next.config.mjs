c9536812f5ab480f0d93f14fef5c2e0f98fbf98d
Souzalift <souzalift@gmail.com>
2025-04-18 02:18:49 -0300

    Add Avatar component and update dependencies for Radix UI Avatar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
